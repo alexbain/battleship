7e6b1318620a461edf4f33fa110dac89ddb67f07
Alex Bain <alex@alexba.in>
2010-09-27 14:58:49 -0700

    Init

---
+++ battleship.js
@@ -0,0 +1,34 @@
+var Boat = function( owner, startCoord, orientation, length ) {
+    
+    var owner = owner;
+    var startCoord = startCoord;
+    var orientation = orientation;
+    var length = length;
+    var boat = [];
+    
+    /* Given a coordinate, return true if the coordinate overlaps the boat's position, false otherwise */
+    function isAt(coord) {
+        
+    }
+    
+    /* Given a coordinate, mark that spot as hit */
+    function takeHit(coord) {     
+        
+    }
+    
+    /* If all pieces of a boat have been hit, return true */
+    function isSunk() {
+        
+    }
+    
+}
+
+function Board = function(owner, size) {
+    
+    var grid = new Array(size);
+    
+    console.log(grid);
+    
+}
+
+var board = new Board('', 5);
